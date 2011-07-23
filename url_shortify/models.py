67257e89a5b356f495982bda85219267c227e2cd
Subhranath Chunder <subhranath@gmail.com>
2011-07-24 03:30:29 +0530

    Basic url shorting model added

--- url_shortify/models.py
+++ url_shortify/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class URL(models.Model):
+    """Class to store a shortened URL data.
+    """
+    original_url = models.URLField(max_length=2000, verify_exists=False)
+    short_string = models.TextField(db_index=True, unique=True, \
+                                    blank=False, null=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+    modified_on = models.DateTimeField(auto_now=True)
+    source_ip = models.GenericIPAddressField(blank=False, null=False)
+    count = models.PositiveIntegerField(default=0, null=False)
+    
+    def __unicode__(self):
+        return unicode(self.short_string)
